2b71f3259428c7db393c61cf9d7e5578ad2efe71
Kat White <kat.white.a@gmail.com>
2021-07-07 13:45:48 -0500

    Add app class component structure and basic JXS

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+
+    }
+  }
+
+  // componentDidMount() {
+  //   fetch('URL')
+  //   .then(response => response.json())
+  //   .then(movies => this.setState({movies: movies}))
+  //   .catch(() => this.setState( {error: "something went wrong"}))
+  // }
+  
+  render() { 
+    return (
+      <body>
+        <nav>
+          
+        </nav>
+        <main>
+
+        </main>
+      </body>
+    );
+  }
 }
+ 
 
 export default App;
