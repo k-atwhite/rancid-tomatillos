80756cebe7a1c6c8d9b66117888b6b5614010655
Kat White <kat.white.a@gmail.com>
2021-07-07 13:56:09 -0500

    Add files for Movies, Movie, css, and data

---
+++ src/Movie.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import './Movie.css'
+
+const Movie = ({MOVIE DETAILS}) => {
+    return (
+        <div>
+            <h3></h3>
+            <p></p>
+        </div>
+    )
+}
