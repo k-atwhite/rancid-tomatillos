2ec6087149710f1f378f5b806ccea8481ab3b288
Kat White <kat.white.a@gmail.com>
2021-07-18 13:06:03 -0400

    Add alt text for accessibility

--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -4,10 +4,9 @@ import { Link } from 'react-router-dom';
 
 const MovieCard = ({ poster, title, id }) => {
     return (
-        <div className='movie-card' style={{backgroundImage: `url(${poster})`}}>
+        <div className='movie-card' alt={`${title}`} style={{backgroundImage: `url(${poster})`}}>
             <Link to={`/movies/${id}`}>
                 <img className="movie-poster" src={poster} alt={'movie poster'}></img>
-                {/* <h4 className="movie-card-title">{title}</h4> */}
             </Link>
         </div>
 
